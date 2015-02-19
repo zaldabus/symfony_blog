61fc6d5b7494b4e0549c364cfa4cfc270c8a16e9
Ryan Beckman <beckman.ryan@gmail.com>
2015-02-19 14:36:32 -0500

    Adds server-side form validation

--- src/Blogger/BlogBundle/Entity/Enquiry.php
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -2,6 +2,12 @@
 
 namespace Blogger\BlogBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\MinLength;
+use Symfony\Component\Validator\Constraints\MaxLength;
+
 class Enquiry
 {
 	protected $name;
@@ -50,5 +56,17 @@ class Enquiry
   public function setBody($body)
   {
       $this->body = $body;
+  }
+
+  public static function loadValidatorMetadata(ClassMetadata $metadata)
+  {
+  	$metadata->addPropertyConstraint('name', new NotBlank());
+
+  	$metadata->addPropertyConstraint('email', new Email());
+
+  	$metadata->addPropertyConstraint('subject', new NotBlank());
+  	$metadata->addPropertyConstraint('subject', new MaxLength(50));
+
+  	$metadata->addPropertyConstraint('body', new MinLength(50));
   }
 }
